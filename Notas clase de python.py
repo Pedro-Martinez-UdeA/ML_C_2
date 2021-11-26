8ab3689f3a8d0489ddd5ece08c49444cc44ffe32
Pedro-Martinez-UdeA <95001049+Pedro-Martinez-UdeA@users.noreply.github.com>
2021-11-26 18:19:47 -0500

    Add files via upload

---
+++ Notas clase de python.py
@@ -0,0 +1,69 @@
+>>> #OPERACIONES MATEMATICAS
+>>> 5+9
+14
+>>> 4*5
+20
+>>> 4/3
+1.3333333333333333
+>>> 4//3
+1
+>>> 4/2
+2.0
+>>> 4%5
+4
+>>> 5%4
+1
+>>> #RESIDUO PARA PARES 0 , RESIDUO 1 PARA IMPARES (CUANSO SE DIVIDEN POR 2)
+>>> 5%2
+1
+>>> 4%2
+0
+>>>## "pow" ES HOMOLOGA A USAR "**" EN UN NUMERO PARA ELEVARLO A UNA POTENCIA
+>>> 3**2
+9
+>>> pow(3,2)
+9
+
+>>> # LAS CADENAS DE TXT SE CARACTERIZAN CON LAS ""/'' EN LA FUNCION PRINT, ESTAS NO SON NECESARIAS PARA OPERACIONES MATEMATICAS
+>>> print(2+3)
+5
+>>> print("2+3")
+2+3
+>>> ########
+>>> n=3
+>>> print(n%2)
+1
+>>> #####
+>>> a,b=3,7
+>>> a
+3
+>>> b
+7
+>>> a,b
+(3, 7)
+>>> a+b
+10
+>>> print(1+2*3)
+7
+>>> # HAY JERARQUIA DE OPERACIONES
+>>>
+>>> #VALORES BOOLEANOS
+>>> True
+True
+>>> False
+False
+>>> 3<2
+False
+>>> 3>2
+True
+>>> ## "diferente de" (' != ')
+>>> ## "diferente de" (' != ')
+>>> ## "igual a" (' == ')
+>>> 3!=1
+True
+>>> 8==3
+False
+>>> 8>=3
+True
+>>> 3>=8
+False
